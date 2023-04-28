ac7967e008296733f92efeaee14a9e6bc1528b7f
Jose Real <115721093+Jrealmorillo@users.noreply.github.com>
2023-04-28 05:44:07 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const suits = ["♦", "♥", "♠", "♣"];
+
+  function randomValue() {
+    return Math.floor(Math.random() * values.length);
+  }
+
+  function randomSuit() {
+    return Math.floor(Math.random() * suits.length);
+  }
+
+  function randomCard() {
+    let value = document.getElementById("value");
+    let newValue = values[randomValue()];
+    value.innerHTML = newValue;
+
+    let suit = document.getElementsByClassName("suit");
+    let newSuit = suits[randomSuit()];
+
+    for (let i = 0; i < suit.length; i++) {
+      suit[i].innerHTML = newSuit;
+      if (newSuit === "♦" || newSuit === "♥") {
+        value.style.color = "red";
+        suit[i].style.color = "red";
+      } else {
+        value.style.color = "black";
+        suit[i].style.color = "black";
+      }
+    }
+    let cardImg = document.querySelector(".card-img");
+    cardImg.classList.add("hidden");
+  }
+
+  let cardImg = document.querySelector(".card-img");
+  cardImg.classList.remove("hidden");
+
+  const button = document.getElementById("new-card-button");
+  button.addEventListener("click", randomCard);
 };
